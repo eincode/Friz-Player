ecb2d30ff7eb9475f5b719b68925c2e440149b55
Princhaa <deras.hujan15@gmail.com>
2017-10-12 13:10:50 +0700

    Added resume and documentation

--- src/services/music.js
+++ src/services/music.js
@@ -5,10 +5,12 @@ var music
 var playlist
 var currentIndexPlaying = null
 
+//Must be called whenever the playlist is changed
 const initializePlayer = (musics) => {
 	playlist = musics
 }
 
+//Play the song with index parameter from the playlist
 const playSong = (index, onFinishedPlaying) => {
 	music = new Sound(playlist[index].path, '', (error) => {
 		if (error) {
@@ -22,27 +24,36 @@ const playSong = (index, onFinishedPlaying) => {
 
 }
 
+//Don't forget to call release() after stopping the song to release the resources used by the player
 const stopSong = (cb) => {
 	music.stop()
 	cb()
 }
 
+//Play the next song from the playlist
 const nextSong = () => {
 	this.stopSong(() => {
 		this.playSong(++currentIndexPlaying)
 	})
 }
 
+//Play the previous song from the playlist
 const previousSong = () => {
 	this.stopSong(() => {
 		this.playSong(--currentIndexPlaying)
 	})
 }
 
+//Pause the currently playing song, call resume() to resume the playback
 const pause = () => {
 	music.pause()
 }
 
+const resume = () => {
+	music.play()
+}
+
+//Release resources used by the player. Only use this when intended to stop the entire playback (not pausing)
 const release = () => {
 	music.release
 }
@@ -52,6 +63,7 @@ module.exports = {
 	playSong,
 	stopSong,
 	nextSong,
+	resume,
 	pause,
 	release,
 	previousSong
